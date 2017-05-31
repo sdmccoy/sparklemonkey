efccacf3d98367bef2253cdf2a4fc5b1966e9064
Scott <sdmccoy@gmail.com>
2017-05-31 16:26:38 -0700

    info window is populating

--- public/scripts/views/map.js
+++ public/scripts/views/map.js
@@ -23,20 +23,23 @@ google.maps.event.addDomListener(window, 'resize', function() {
 
 // start of marker clusters
 function initMap(concerts) {
-  console.log('test1');
+  console.log(concerts);
   // Create an array of alphabetical characters used to label the markers.
   var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var noteIcon = 'http://www.kindermusik.co.za/site/wp-content/uploads/2015/12/icon-music-note-kindermusik-blue-2000x2000-2000x2000-e1448965274640.png';
-  console.log(concerts);
+  var windowContent = concerts.map(function(concert){
+    console.log();
+    return `Event: ${concert.name}<br>Date: ${concert.date}<br>Time: ${concert.time}<br>Venue: ${concert.venue}<br><a href="${concert.url}" target="_blank">Buy Tickets</a>`
+  });
 
   // Add some markers to the map.
   // Note: The code uses the JavaScript Array.prototype.map() method to
   // create an array of markers based on a given 'locations' array.
   // The map() method here has nothing to do with the Google Maps API.
   var locations = concerts.map(function(concert){
-    console.log('test3');
     return {lat: parseFloat(concert.venueLat), lng: parseFloat(concert.venueLong)};
   });
+
 
   var markers = locations.map(function(location, i) {
     return new google.maps.Marker({
@@ -51,7 +54,16 @@ function initMap(concerts) {
   var markerCluster = new MarkerClusterer(map, markers,
       {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
+  markers.forEach(function(marker, index){
+    var infoWindow = new google.maps.InfoWindow({
+      content: windowContent[index]
+    });
+    marker.addListener('click', function(event){
+      console.log(event);
+      infoWindow.open(map, marker);
+    })
+  });
 }
 app.Concert.fetchAll(app.Concert.defaultParams, function(){
   initMap(app.Concert.all);
-})
+});
