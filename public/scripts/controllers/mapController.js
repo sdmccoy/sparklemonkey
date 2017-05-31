b8bf93babcc2fff59499f6dffdee57906106961b
Megan Flood <mak.flood@comcast.net>
2017-05-31 11:18:56 -0700

    added a handler for form submission, added middleware and route to capture the form data.

--- public/scripts/controllers/mapController.js
+++ public/scripts/controllers/mapController.js
@@ -5,9 +5,25 @@ var app = app || {};
 (function(module){
   const mapController = {};
 
+  const f = $('form')[0];
+
+  mapController.createFilter = function(ctx, next) {
+    let start = ctx.params.start.split('-');
+    let end = ctx.params.end.split('-');
+    ctx.filter = {
+      postalCode: ctx.params.area,
+      startDateTime: new Date(start[0], parseInt(start[1]) - 1, start[2], 0).toISOString().replace(/\.\d\d\d/, ''),
+      endDateTime: new Date(end[0], parseInt(end[1]) - 1, end[2], 23, 59).toISOString().replace(/\.\d\d\d/, ''),
+      classificationName: 'Music'
+    }
+    console.log(ctx.filter);
+    next();
+  };
+
   mapController.index = function(){
     $('.button-content').hide();
     $('#map').show();
+    app.mapView.initPage();
   };
   module.mapController = mapController;
 })(app);
--- public/scripts/views/mapView.js
+++ public/scripts/views/mapView.js
@@ -6,6 +6,36 @@ var app = app || {};
 
   const mapView = {};
 
+  const f = $('form')[0];
+
+  mapView.handleFilterFormSubmit = function() {
+    $('form').off('submit').on('submit', function(e) {
+      console.log('submit');
+      e.preventDefault();
+      let path = [f.area.value, f.startDate.value, f.endDate.value].join('/');
+      page.show(`/${path}`);
+    })
+  }
+
+  mapView.setDefaults = function() {
+    f.area.value = '98121';
+    let today = new Date();
+    let month = `${today.getMonth() + 1}`.length < 2 ? `0${today.getMonth() + 1}` : today.getMonth() + 1;
+    let day = `${today.getDate()}`.length < 2 ? `0${today.getDate()}` : today.getDate();
+    f.startDate.value = `${today.getFullYear()}-${month}-${day}`;
+    let weekAway = new Date(today.setDate(today.getDate() + 7));
+    month = `${weekAway.getMonth() + 1}`.length < 2 ? `0${weekAway.getMonth() + 1}` : weekAway.getMonth() + 1;
+    day = `${weekAway.getDate()}`.length < 2 ? `0${weekAway.getDate()}` : weekAway.getDate();
+    f.endDate.value = `${weekAway.getFullYear()}-${month}-${day}`;
+  }
+
+  mapView.initPage = function() {
+    if (!f.area.value && !f.startDate.value && !f.endDate.value) {
+      mapView.setDefaults();
+    }
+    mapView.handleFilterFormSubmit();
+  }
+
   mapView.initMap = function(){
     const map = new google.maps.Map($('#map'), app.mapOptions);
 
