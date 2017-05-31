6cffd3742841d7488e53dfa5873aa742c1794539
Megan Flood <mak.flood@comcast.net>
2017-05-31 11:58:26 -0700

    moved form contolling items to generalView and generalController. also sorted the concert list to be in chronological order.

--- public/scripts/controllers/mapController.js
+++ public/scripts/controllers/mapController.js
@@ -5,21 +5,6 @@ var app = app || {};
 (function(module){
   const mapController = {};
 
-  const f = $('form')[0];
-
-  mapController.createFilter = function(ctx, next) {
-    let start = ctx.params.start.split('-');
-    let end = ctx.params.end.split('-');
-    ctx.filter = {
-      postalCode: ctx.params.area,
-      startDateTime: new Date(start[0], parseInt(start[1]) - 1, start[2], 0).toISOString().replace(/\.\d\d\d/, ''),
-      endDateTime: new Date(end[0], parseInt(end[1]) - 1, end[2], 23, 59).toISOString().replace(/\.\d\d\d/, ''),
-      classificationName: 'Music'
-    }
-    console.log(ctx.filter);
-    next();
-  };
-
   mapController.index = function(){
     $('.button-content').hide();
     $('#map').show();
