1e2fd38a9e153fcc6983804c58031eb215a1b76f
Scott <sdmccoy@gmail.com>
2017-05-31 21:24:51 -0700

    when a user clicks on a marker that has duplicate locations it will split apart and present multiple markers

--- public/scripts/views/map.js
+++ public/scripts/views/map.js
@@ -13,51 +13,62 @@ var mapOptions = {
 }
 
 // launches the map
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+let map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-google.maps.event.addDomListener(window, 'resize', function() {
-  var center = map.getCenter();
-  google.maps.event.trigger(map, 'resize');
-  map.setCenter(center);
-});
+// google.maps.event.addDomListener(window, 'resize', function() {
+//   var center = map.getCenter();
+//   google.maps.event.trigger(map, 'resize');
+//   map.setCenter(center);
+// });
 
 // start of marker clusters
 function initMap(concerts) {
   // Create an array of alphabetical characters used to label the markers.
-  var noteIcon = $('#style-selector').val() === 'dark-mode' || $('#style-selector').val() === 'night-mode' ? '/img/note-icon.png' : '/img/dark-note-icon.png';
-  var windowContent = concerts.map(function(concert){
-    return `<div id="window-content" style="font-size:20px"><strong>Event:</strong> ${concert.name}<br><strong>Date: </strong>${concert.date}<br><strong>Time: </strong>${concert.time}<br><strong>Venue: </strong>${concert.venue}<br><strong><a href="${concert.url}" target="_blank" style="color: blue;">Click to Buy Tickets</a></strong></div>`
+  let noteIcon = $('#style-selector').val() === 'dark-mode' || $('#style-selector').val() === 'night-mode' ? '/img/note-icon.png' : '/img/dark-note-icon.png';
+  // starting the spiderfier library to assist with identical location markers.
+  let oms = new OverlappingMarkerSpiderfier(map, {
+    markersWontMove: true,
+    markersWontHide: true,
+    basicFormatEvents: true
   });
-
-  // Add some markers to the map.
-  // Note: The code uses the JavaScript Array.prototype.map() method to
-  // create an array of markers based on a given 'locations' array.
-  // The map() method here has nothing to do with the Google Maps API.
-  var locations = concerts.map(function(concert){
-    return {lat: parseFloat(concert.venueLat), lng: parseFloat(concert.venueLong)};
-  });
-
-
-  var markers = locations.map(function(location) {
-    // console.log(locations);
-    return new google.maps.Marker({
-      position: location,
-      map: map,
+  // builds the markers based on the user form input.
+  let markers = concerts.map(function(concert){
+    let locations = {lat: parseFloat(concert.venueLat), lng: parseFloat(concert.venueLong)};
+    let marker = new google.maps.Marker({
+      position: locations,
       icon: noteIcon,
       animation: google.maps.Animation.DROP
     });
+    //builds the info window when the user clicks on the location marker
+    let windowContent = `<div id="window-content" style="font-size:20px"><strong>Event:</strong> ${concert.name}<br><strong>Date: </strong>${concert.date}<br><strong>Time: </strong>${concert.time}<br><strong>Venue: </strong>${concert.venue}<br><strong><a href="${concert.url}" target="_blank" style="color: blue;">Click to Buy Tickets</a></strong></div>`
+    let infoWindow = new google.maps.InfoWindow({
+      content: windowContent
+    });
+    google.maps.event.addListener(marker, 'spider_click', function(e){
+      infoWindow.open(map, marker);
+    });
+    oms.addMarker(marker);
   });
 
+  // var markers = locations.map(function(location) {
+  //   return new google.maps.Marker({
+  //     position: location,
+  //     map: map,
+  //     icon: noteIcon,
+  //     animation: google.maps.Animation.DROP
+  //   });
+  // });
+
   // Add a marker clusterer to manage the markers.
-  // var markerCluster = new MarkerClusterer(map, markers,
-  //     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+  let markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
   // This displays the info box when you click on the marker.
-  markers.forEach(function(marker, index){
-    var infoWindow = new google.maps.InfoWindow({
-      content: windowContent[index]
-    });
-    marker.addListener('click', function(){
-      infoWindow.open(map, marker);
-    })
-  });
+  // markers.forEach(function(marker, index){
+  //   var infoWindow = new google.maps.InfoWindow({
+  //     content: windowContent[index]
+  //   });
+  //   marker.addListener('click', function(){
+  //     infoWindow.open(map, marker);
+  //   })
+  // });
 }
